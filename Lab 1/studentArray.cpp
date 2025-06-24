cdf22b8fca1cd87707e4ef890cb5373481d913bf
ArKizame <149134474+ArKizame@users.noreply.github.com>
2025-06-25 03:09:00 +0800

    Add files via upload

---
+++ Lab 1/studentArray.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct Student {
+    int id;
+    string name;
+    string email;
+    string phone;
+};
+
+int main() {
+    Student students[5];
+
+    for (int i = 0; i < 5; ++i) {
+        cout << "Enter details for student " << i + 1 << ":\n";
+        cout << "ID: ";
+        cin >> students[i].id;
+        cin.ignore();
+        cout << "Name: ";
+        getline(cin, students[i].name);
+        cout << "Email: ";
+        getline(cin, students[i].email);
+        cout << "Phone: ";
+        getline(cin, students[i].phone);
+    }
+
+    cout << "\nStudent Information:\n";
+    for (int i = 0; i < 5; ++i) {
+        cout << "----------------------\n";
+        cout << "Student " << i + 1 << endl;
+        cout << "ID: " << students[i].id << endl;
+        cout << "Name: " << students[i].name << endl;
+        cout << "Email: " << students[i].email << endl;
+        cout << "Phone: " << students[i].phone << endl;
+    }
+
+    return 0;
+}
